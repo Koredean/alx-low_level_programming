2a8d9d127cff532155f32d98c59c72d47a17435e
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-08 18:05:17 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+}
