75c5970a856d7e01ed274647308fdafa5213a986
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-08 18:16:29 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0)
+}
