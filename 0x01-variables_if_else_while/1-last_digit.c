7aa9ff7882604531bcba7d8a70ba7d1212558157
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-11 13:49:19 -0700

    0ne

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,9 +11,7 @@ int main(void)
 	int n;
 	char last[] = "Last digit of";
 
-	srand(time(0))
-
-
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and", last, n, n % 10);
 	if (n % 10 > 5)
