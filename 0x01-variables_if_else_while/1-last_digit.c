9d0159bc23d3e98915b3e6b2c9abd1c5213e2715
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-11 13:59:09 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	printf("%s %d is %d and", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("0\n");
+		printf("Last digit of %d is %d and is 0\n");
 	}
 	else
 	{
-		printf("less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n");
 	}
 	return (0);
 }
