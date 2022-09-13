ca5d17b1979a7913c2e43ff3de8f414735f77a44
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-12 19:25:34 -0700

    112

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
-/*
- * main - Prints possible combination of two digit numbers
+/**
+ * main - Prints possible combination of two different sigits
  *
+ * Description: using the main function
+ * this programm prints possible combination of two dgit numbers
  * Return: Always (Success)
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
 
-	while (c <= 99)
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
 
-		if (f_d < l_d)
+		for (j = '1'; j <= '9'; j++)
 		{
-			putchar(f_d);
-			putchar(l_d);
 
-			if (c != 89)
+			if (j > i)
 			{
-				putchar(',');
-				putchar(' ');
+
+				putchar(j + '0');
+				putchar(i + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-		c++;
 	}
 	putchar('\n');
+
 	return (0);
 }
+
+
+
+
