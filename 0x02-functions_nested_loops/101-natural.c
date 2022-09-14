a131d2af3959595912324cf467740dee776ddd5f
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-14 14:39:53 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,41 +1,17 @@
-#include "main.h"
+#include <stdio.h>
 /**
-*print_times_table - prints the  times table with
-*parameter
-*@n: parameter
-*Return: returns nothing
+*main - prints count of multiples
+*of 3 or 5 below 1024
+*Return: return 0
 */
-void print_times_table(int n)
+int main(void)
 {
-int digit, mult, result;
-if (n <= 15 && n >= 0)
+int n, sum = 0;
+for (n = 0; n < 1024; n++)
 {
-for (digit = 0; digit <= n; digit++)
-{
-_putchar('0');
-
-for (mult = 1; mult <= n; mult++)
-{
-_putchar(',');
-_putchar(' ');
-result = digit * mult;
-if (result <= 99)
-_putchar(' ');
-
-if (result <= 9)
-_putchar(' ');
-if (result >= 100)
-{
-_putchar((result / 100) + '0');
-_putchar((result / 10) % 10 + '0');
+if ((n % 3) == 0 || (n % 5) == 0)
+sum += n;
 }
-else if (result <= 99 && result >= 10)
-{
-_putchar((result / 10) + '0');
-}
-_putchar((result % 10) + '0');
-}
-_putchar('\n');
-}
-}
+printf("%d\n", sum);
+return (0);
 }
