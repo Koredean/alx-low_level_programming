c65f47f60d7b200ed26ccfc34fd5193c9bd0f4ab
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-22 02:50:09 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenate string src to the end of string dest with
+ * inputed number of bytes
+ * @dest: first string
+ * @src: second string
+ * @n: integer parameter to compareindex to
+ * Return: returns new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+
+}
+
