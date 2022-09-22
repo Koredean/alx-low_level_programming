cb1c93eb6471c44091be2673b01bf5d1decc37c9
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-22 06:56:29 -0700

    new task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *  _strcmp - function which compare two strings and
- * @s1: first string
- * @s2: second string
- * Return:
- *          retuns zero if s1 == s2
- *          returns negative number if s1 < s2
- *          returns positive number if s1 > s2
- */
+*  _strcmp - Function which compare two strings and
+*@s1: first string
+*@s2:second string
+*Return:
+*		returns zero if s1 == s2
+*		returns negative number if s1 < s2
+*		returns positive number if s1 > s2
+*/
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff i = 0;
+	int i = 0, diff = 0;
 
 	while (1)
 	{
@@ -35,8 +35,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 			i++;
+
 	}
 	return (diff);
 }
-
-
