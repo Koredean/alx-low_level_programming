d5387e27a57ada967824dc3de7a688b1b00fbf99
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-22 00:21:46 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,13 @@
 char *_strcat(char *dest, char *src);
 {
 
-	/*concatenates char *dest and char *src*/
-	strcat(dest, src)
-		printf("strcat(dest, src): %s/n", dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
