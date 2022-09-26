45412d5c3212f3d49efab6732a6d6329168920bb
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-09-26 16:54:15 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills a memmory block with a constant byte
+ * @s: Address to memory block
+ * @b: char to be used
+ * @n: number of byte to be used
+ *
+ * Return: pointer to the memory block
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s(n - 1) = b;
+		n++
+	}
+	return (s);
+}
