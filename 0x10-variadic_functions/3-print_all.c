0cf9c53c0a3172e5d4eed68c3557e15a18cedffa
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-10-15 05:38:50 -0700

    new 3 again

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,10 +3,10 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything
- * @format: a list of types of arguments passed to the function
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
- * Return: no return
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
