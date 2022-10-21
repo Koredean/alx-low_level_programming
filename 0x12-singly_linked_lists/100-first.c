0e286d1bf8b47fc77771f1f57d284e42e72ca16a
OjumoolaOluwakorede <ojumoolaoluwakorede@gmail.com>
2022-10-20 17:04:07 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void) __attrbute__((constuctor));
+
+/**
+ * first - prints a string before the main function is executed
+ *
+ * Return: void';lpkrwbnrrehnlkfbr
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
